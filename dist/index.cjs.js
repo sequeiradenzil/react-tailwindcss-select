ae5ddd3ed178ac580cc27468f655206681388df1
sequeiradenzil <sequeira.denzil@gmail.com>
2022-11-05 06:12:43 +0530

    testing z-index change

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -187,6 +187,21 @@ const Select = ({ options = [], value = null, onChange, placeholder = "Select...
             }
             if (isMultiple && (Array.isArray(value) || value === null)) {
                 onChange(value === null ? [selected] : [...value, selected]);
+                const filterdiv = document.getElementById('filterdiv');
+                if (value !== null && filterdiv != null) {
+                    let addhtml = "   <span\n" +
+                        "                                className=\"inline-flex items-center rounded-full bg-indigo-100 py-0.5 pl-2.5 pr-1 text-sm font-medium text-indigo-700\">\n" +
+                        +value +
+                        "  <button type=\"button\"\n" +
+                        "          className=\"ml-0.5 inline-flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:bg-indigo-500 focus:text-white focus:outline-none\">\n" +
+                        "    <span className=\"sr-only\">Remove large option</span>\n" +
+                        "    <svg className=\"h-2 w-2\" stroke=\"currentColor\" fill=\"none\" viewBox=\"0 0 8 8\">\n" +
+                        "      <path strokeLinecap=\"round\" strokeWidth=\"1.5\" d=\"M1 1l6 6m0-6L1 7\"/>\n" +
+                        "    </svg>\n" +
+                        "  </button>\n" +
+                        "</span>";
+                    filterdiv.innerHTML += addhtml;
+                }
             }
         }
         if (selected !== value) {
@@ -207,7 +222,7 @@ const Select = ({ options = [], value = null, onChange, placeholder = "Select...
     return (React__default["default"].createElement(SelectProvider, { value: value, handleValueChange: handleValueChange },
         React__default["default"].createElement("div", { className: "relative w-full", ref: ref },
             React__default["default"].createElement("div", { tabIndex: 0, "aria-expanded": open, onClick: toggle, className: `flex text-sm text-gray-500 border border-gray-300 rounded shadow-sm transition duration-300 focus:outline-none${isDisabled ? ' bg-gray-200' : ' bg-white hover:border-gray-400 focus:ring-2 focus:ring-blue-500'}` },
-                React__default["default"].createElement("input", { className: "grow pl-2.5 py-2 pr-2 flex flex-wrap gap-1", type: "text", value: inputValue, onChange: e => setInputValue(e.target.value), placeholder: placeholder, name: "" }),
+                React__default["default"].createElement("input", { className: "grow pl-2.5 py-2 pr-2 flex flex-wrap gap-1", type: "text", value: inputValue, onChange: e => setInputValue(e.target.value), placeholder: placeholder }),
                 React__default["default"].createElement("div", { className: "flex flex-none items-center py-1.5" },
                     loading && (React__default["default"].createElement("div", { className: "px-1.5" },
                         React__default["default"].createElement(Spinner, null))),
