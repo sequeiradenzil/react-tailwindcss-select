620e0fc95d8d4db4e9c58b09fe843440176669cd
sequeiradenzil <sequeira.denzil@gmail.com>
2022-11-07 05:58:15 +0530

    testing z-index change

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -82,23 +82,22 @@ const Select: React.FC<SelectProps> = ({options = [], value = null, onChange, pl
 
             if (isMultiple && (Array.isArray(value) || value === null)) {
                   const filterdiv = document.getElementById('filterdiv');
-        if (selected.value !== null && filterdiv != null) {
-            let addhtml = "   <span\n" +
-                "                                className=\"inline-flex items-center rounded-full bg-indigo-100 py-0.5 pl-2.5 pr-1 text-sm font-medium text-indigo-700\">\n" +
-                +selected.value +
-                "  <button type=\"button\"\n" +
-                "          className=\"ml-0.5 inline-flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:bg-indigo-500 focus:text-white focus:outline-none\">\n" +
-                "    <span className=\"sr-only\">Remove large option</span>\n" +
-                "    <svg className=\"h-2 w-2\" stroke=\"currentColor\" fill=\"none\" viewBox=\"0 0 8 8\">\n" +
-                "      <path strokeLinecap=\"round\" strokeWidth=\"1.5\" d=\"M1 1l6 6m0-6L1 7\"/>\n" +
-                "    </svg>\n" +
-                "  </button>\n" +
-                "</span>";
-            filterdiv.innerHTML += addhtml;
-        }
-
                 onChange(value === null ? [selected] : [...value, selected]);
             }
+               const filterdiv = document.getElementById('filterdiv');
+                if (selected.label !== null && filterdiv !== null) {
+                    let addhtml = "   <span\n" +
+                        "                                class=\"inline-flex items-center rounded-full bg-indigo-100 py-0.5 pl-2.5 pr-1 text-sm font-medium text-indigo-700\">\n"+ selected.label +
+                        "  <button type=\"button\"\n" +
+                        "          class=\"ml-0.5 inline-flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:bg-indigo-500 focus:text-white focus:outline-none\">\n" +
+                        "    <span class=\"sr-only\">Remove large option</span>\n" +
+                        "    <svg class=\"h-2 w-2\" stroke=\"currentColor\" fill=\"none\" viewBox=\"0 0 8 8\">\n" +
+                        "      <path strokeLinecap=\"round\" strokeWidth=\"1.5\" d=\"M1 1l6 6m0-6L1 7\"/>\n" +
+                        "    </svg>\n" +
+                        "  </button>\n" +
+                        "</span>";
+                    filterdiv.innerHTML += addhtml;
+                }
         }
 
         if (selected !== value) {
