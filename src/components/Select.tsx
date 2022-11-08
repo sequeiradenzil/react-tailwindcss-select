c41c5f89f3d3ca1fb9205742bd25153b90241be3
sequeiradenzil <sequeira.denzil@gmail.com>
2022-11-08 08:21:59 +0530

    testing z-index change

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -101,7 +101,7 @@ const Select: React.FC<SelectProps> = ({
             const filterdiv = document.getElementById('filterdiv');
             if (selected.label !== null && filterdiv !== null) {
                 let addhtml = " <span class=\"mr-5 inline-flex items-center rounded-full bg-indigo-100 py-0.5 pl-2.5 pr-1 text-sm font-medium text-indigo-700\">\n"+ selected.label+
-                    "                     <button  onClick=\"{e => removeItem(e, selected)}\"   type=\"button\"\n" +
+                    "                     <button  onclick=\"handleremoveItem(e, selected)\"  type=\"button\"\n" +
                     "                              class=\"ml-0.5 inline-flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:bg-indigo-500 focus:text-white focus:outline-none\"> +\n" +
                     "                        <span class=\"sr-only\">Remove large option</span>\n" +
                     "                        <svg class=\"h-2 w-2\" stroke=\"currentColor\" fill=\"none\" viewBox=\"0 0 8 8\">\n" +
@@ -130,6 +130,10 @@ const Select: React.FC<SelectProps> = ({
             onChange(result.length ? result : null);
         }
     }, [isMultiple, onChange, value]);
+
+    function handleremoveItem(e: React.MouseEvent<HTMLDivElement>, item: Option) {
+        removeItem(e, item)
+    }
 
 
     return (
